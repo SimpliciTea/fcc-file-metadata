1f8a0b90e0673946cbd1d70cf008849aeda1a7a7
John Ley <ley.johnt@gmail.com>
2016-08-24 15:26:00 -0700

    functioning locally, pushing to Heroku

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,37 @@
 'use strict';
 
 var path = process.cwd();
+var multer = require('multer');
+
+/*var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path + '/tmp/uploads')
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now());
+	}
+})*/
+
+// storing in memory as we have no use for persistent storage
+var storage = multer.memoryStorage();
+
+var upload = multer({ storage: storage });
 
 module.exports = function (app) {
 	app.route('/')
 		.get(function(req, res) {
 			res.sendFile(path + '/public/index.html');
+		});
+
+	app.route('/upload')
+		.post(upload.single('userFile'), function(req, res, next) {
+			var metadata = {
+				size: req.file.size
+			};
+
+			res.send(metadata);
+		})
+		.get(function(req, res) {
+			res.sendFile(path + '/public/upload.html');
 		});
 };
